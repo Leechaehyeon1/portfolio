e7b71587eabdefc1e86f9a68b71deb87c69d4ff6
Leechaehyeon1 <lch09253@hanmail.net>
2021-09-27 21:21:10 +0900

    landing page

--- js/custom.js
+++ js/custom.js
@@ -33,22 +33,37 @@ $(document).ready(function(){
       $('body').addClass('on');
       $(this).addClass('on');
       TweenMax.staggerTo('.gnb a', 0.8,
-      { y: '0%', delay:1 },
+      { y: '0%', delay: 1 },
         0.2
       );
     }
   })
   
   // gnb_btn click event
-  $('.gnb a').on('click', function(e){
+  let menuTab = $('.gnb ul li');
+  menuTab.on('click', function(e){
     e.preventDefault();
 
-    $('.gnb a').removeClass('on');
-    $(this).addClass('on');
-    var curr = $('.container').scrollTop();
-    var target = $(this).data('target');
-    var total = $('.'+target).offset().top + curr;
-    $('.container').animate({scrollTop: total}, 300);
+    menuTab.children('a').removeClass('on');
+    
+    let i = $(this).index();
+    $(this).children('a').addClass('on');
+    
+    let curr = $('.container').scrollTop();
+    let curOffset =  $('.container .section').eq(i).offset().top;
+    let total = curOffset + curr;
+    $('.container').animate({scrollTop: total}, 500);
   });
 
+  // scroll gnb toggl class event
+  $('.container').on('scroll', function(){
+    $('.container .section').each(function(){
+      if($(this).offset().top <= $(window).scrollTop()){
+        let i = $(this).index();
+
+      menuTab.children('a').removeClass('on');
+      menuTab.eq(i).children('a').addClass('on');
+      }
+    });
+  });
 });
